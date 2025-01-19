751ff09313f5e46a38589d1bd889a4523590350f
gustavoventieri <ventierigustavo@gmail.com>
2025-01-19 06:02:10 -0300

    feat: admin and user repository added

---
+++ src/main/java/gustavo/ventieri/capitalmind/repository/AdminRepository.java
@@ -0,0 +1,12 @@
+package gustavo.ventieri.capitalmind.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gustavo.ventieri.capitalmind.entity.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, UUID> {
+
+    
+} 
---
+++ src/main/java/gustavo/ventieri/capitalmind/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package gustavo.ventieri.capitalmind.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gustavo.ventieri.capitalmind.entity.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+} 
