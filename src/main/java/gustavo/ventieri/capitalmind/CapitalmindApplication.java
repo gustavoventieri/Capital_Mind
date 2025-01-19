ce7a715e28250439112d0397016e11782d5ad847
gustavoventieri <ventierigustavo@gmail.com>
2025-01-19 01:11:16 -0300

    feat: dotenv added

---
+++ src/main/java/gustavo/ventieri/capitalmind/CapitalmindApplication.java
@@ -0,0 +1,13 @@
+package gustavo.ventieri.capitalmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalmindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gustavo/ventieri/capitalmind/CapitalmindApplicationTests.java
@@ -0,0 +1,13 @@
+package gustavo.ventieri.capitalmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
