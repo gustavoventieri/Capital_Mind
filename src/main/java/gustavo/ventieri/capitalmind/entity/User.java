c4ac08e82a2ecca49fdce3cf4e4a6dc4f521bd70
gustavoventieri <ventierigustavo@gmail.com>
2025-01-19 06:01:24 -0300

    feat: admin and user entity added

---
+++ src/main/java/gustavo/ventieri/capitalmind/entity/User.java
@@ -0,0 +1,120 @@
+package gustavo.ventieri.capitalmind.entity;
+
+
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_Id")
+    private UUID userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "salary")
+    private Integer salary;
+
+    @Column(name = "createAt")
+    @CreationTimestamp
+    private Instant createAt;
+
+    @Column(name = "updateAt")
+    @UpdateTimestamp
+    private Instant updateAt;
+
+    public User() {
+    }
+
+    public User(UUID userId, String name, String email, String password, Integer salary, Instant createAt,
+            Instant updateAt) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.salary = salary;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Instant getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Instant createAt) {
+        this.createAt = createAt;
+    }
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Instant updateAt) {
+        this.updateAt = updateAt;
+    }
+
+
+
+
+    
+}
