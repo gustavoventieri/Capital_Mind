2316f29d1fe7d583736ac7a38e4cb81bb63a8ecc
gustavoventieri <ventierigustavo@gmail.com>
2025-01-31 14:44:36 -0300

    feat: annotation enable open feign added on main class

--- src/main/java/gustavo/ventieri/capitalmind/CapitalmindApplication.java
+++ src/main/java/gustavo/ventieri/capitalmind/CapitalmindApplication.java
@@ -2,8 +2,10 @@ package gustavo.ventieri.capitalmind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CapitalmindApplication {
 
 	public static void main(String[] args) {
