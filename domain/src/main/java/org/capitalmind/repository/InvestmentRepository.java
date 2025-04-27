0098d640a995da1811cd0b753657ed468ed19f1a
gustavoventieri <ventierigustavo@gmail.com>
2025-04-26 23:33:04 -0300

    feat: find all by user data added

--- domain/src/main/java/org/capitalmind/repository/InvestmentRepository.java
+++ domain/src/main/java/org/capitalmind/repository/InvestmentRepository.java
@@ -1,9 +1,11 @@
 package org.capitalmind.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.capitalmind.entity.Investment;
+import org.capitalmind.entity.User;
 
 
 public interface InvestmentRepository {
@@ -12,6 +14,8 @@ public interface InvestmentRepository {
     Investment update(Investment investment);
 
     Optional<Investment> findById(Long investmentId);
+
+    List<Investment> findAllByUserData(User user);
 
     void delete(String investmentId);
 }
