a2ff4899473ac2f2ab3b6019a6ccb3b8fff8d29f
gustavoventieri <ventierigustavo@gmail.com>
2025-04-27 20:22:44 -0300

    feat: all crud functions implemented

--- framework/src/main/java/org/capitalmind/driver/repository/InvestmentRepositoryImpl.java
+++ framework/src/main/java/org/capitalmind/driver/repository/InvestmentRepositoryImpl.java
@@ -1,9 +1,13 @@
 package org.capitalmind.driver.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.capitalmind.driver.repository.client.InvestmentRepositoryOrm;
 import org.capitalmind.entity.Investment;
+import org.capitalmind.entity.User;
+import org.capitalmind.exception.InternalServerError;
+import org.capitalmind.exception.NotFound;
 import org.capitalmind.repository.InvestmentRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,26 +22,53 @@ public class InvestmentRepositoryImpl implements InvestmentRepository{
 
     @Override
     public Investment save(Investment investment) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+       try {
+            return this.investmentRepositoryOrm.save(investment);
+       } catch (Exception exc) {
+            throw new InternalError(exc);
+       }
     }
 
     @Override
     public Investment update(Investment investment) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        try {
+            return this.investmentRepositoryOrm.save(investment);
+       } catch (Exception exc) {
+            throw new InternalError(exc);
+       }
     }
 
     @Override
     public Optional<Investment> findById(Long investmentId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+      try {
+            return this.investmentRepositoryOrm.findById(investmentId);
+       } catch (NotFound exc) {
+            throw new NotFound(exc);
+       } catch (Exception exc){
+            throw new InternalServerError(exc);
+       }
     }
 
     @Override
-    public void delete(String investmentId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    public void delete(Long investmentId) {
+       try {
+            investmentRepositoryOrm.deleteById(investmentId);
+       } catch (NotFound exc) {
+           throw new NotFound(exc);
+       } catch (Exception exc){
+            throw new InternalServerError(exc);
+       }
+    }
+
+    @Override
+    public List<Investment> findAllByUserData(User user) {
+       try {
+            return this.investmentRepositoryOrm.findAllByUserData(user); 
+       } catch (NotFound exc) {
+            throw new NotFound(exc);
+       } catch (Exception exc){
+            throw new InternalServerError(exc);
+       }
     }
     
 }
