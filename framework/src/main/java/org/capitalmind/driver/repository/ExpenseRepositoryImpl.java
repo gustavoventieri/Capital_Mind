4482e64001318a29ac353be53d79aed5c39b6fc8
gustavoventieri <ventierigustavo@gmail.com>
2025-04-26 21:33:06 -0300

    feat: crud functions implemented

--- framework/src/main/java/org/capitalmind/driver/repository/ExpenseRepositoryImpl.java
+++ framework/src/main/java/org/capitalmind/driver/repository/ExpenseRepositoryImpl.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.capitalmind.driver.repository.client.ExpenseRepositoryOrm;
 import org.capitalmind.entity.Expense;
+import org.capitalmind.exception.InternalServerError;
+import org.capitalmind.exception.NotFound;
 import org.capitalmind.repository.ExpenseRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,26 +20,45 @@ public class ExpenseRepositoryImpl implements ExpenseRepository {
 
     @Override
     public Expense save(Expense expense) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        try {
+            Expense newExpense = expenseRepositoryOrm.save(expense);
+            return newExpense;
+        } catch (Exception exc) {
+           throw new InternalServerError(exc);
+        }
     }
 
     @Override
     public Expense update(Expense expense) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+          try {
+            Expense expenseUpdated = expenseRepositoryOrm.save(expense);
+            return expenseUpdated;
+        } catch (Exception exc) {
+           throw new InternalServerError(exc);
+        }
     }
 
     @Override
     public Optional<Expense> findById(Long expenseId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    try {
+        Optional<Expense> expense = expenseRepositoryOrm.findById(expenseId);
+        return expense;
+       } catch (NotFound exc) {
+            throw exc;
+       } catch (Exception exc){
+            throw new InternalServerError(exc);
+       }
     }
 
     @Override
-    public void delete(String expenseId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    public void delete(Long expenseId) {
+       try {
+            expenseRepositoryOrm.deleteById(expenseId);
+       } catch (NotFound exc) {
+            throw exc;
+       } catch (Exception exc){
+            throw new InternalServerError(exc);
+       }
     }
     
 }
