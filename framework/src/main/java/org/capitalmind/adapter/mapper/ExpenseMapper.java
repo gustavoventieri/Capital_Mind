e87039155a5a52224e30a3b99dcf676bfd73e247
gustavoventieri <ventierigustavo@gmail.com>
2025-04-26 23:13:16 -0300

    feat: to expense response added (expense domain  to expense response)

--- framework/src/main/java/org/capitalmind/adapter/mapper/ExpenseMapper.java
+++ framework/src/main/java/org/capitalmind/adapter/mapper/ExpenseMapper.java
@@ -1,7 +1,9 @@
 package org.capitalmind.adapter.mapper;
 
 import org.capitalmind.adapter.dto.request.ExpenseRequestImpl;
+import org.capitalmind.dto.response.ExpenseResponse;
 import org.capitalmind.dto.request.ExpenseRequest;
+import org.capitalmind.entity.Expense;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,4 +13,7 @@ public class ExpenseMapper {
         return new ExpenseRequest(expenseRequestImpl.name(), expenseRequestImpl.description(), expenseRequestImpl.category(), expenseRequestImpl.price(), expenseRequestImpl.userId());
     }
 
+    public ExpenseResponse toExpenseResponse(Expense expense){
+        return new ExpenseResponse(expense.getExpenseId(), expense.getName(), expense.getDescription(), expense.getCategory(), expense.getPrice());
+    }
 }
