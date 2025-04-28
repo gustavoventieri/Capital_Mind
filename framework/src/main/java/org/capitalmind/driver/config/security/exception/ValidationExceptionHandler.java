3e3a0814eb968c878bea4f06ecf1fae315b62956
gustavoventieri <ventierigustavo@gmail.com>
2025-04-27 21:53:54 -0300

    feat: entry point added for validating requests

---
+++ framework/src/main/java/org/capitalmind/driver/config/security/exception/ValidationExceptionHandler.java
@@ -0,0 +1,34 @@
+package org.capitalmind.driver.config.security.exception;
+
+import org.capitalmind.adapter.controller.AuthController;
+import org.capitalmind.adapter.controller.ExpenseController;
+import org.capitalmind.adapter.controller.InvestmentController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice(assignableTypes = { 
+    AuthController.class,
+    ExpenseController.class, 
+    InvestmentController.class  })
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
